27a3604c5d0f70eec0005c0bc1a1a0930a8eff36
EnriquePallares <eapallaress@gmail.com>
2018-09-06 21:10:16 -0500

    App terminada sin validaciones

--- app/src/main/java/com/example/android/personasmovil/Listar.java
+++ app/src/main/java/com/example/android/personasmovil/Listar.java
@@ -2,12 +2,43 @@ package com.example.android.personasmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Listar extends AppCompatActivity {
+
+    private TableLayout tabla;
+    private ArrayList<Persona> personas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
+
+        tabla = findViewById(R.id.tabla);
+        personas = Datos.obtener();
+
+        for (int i = 0; i < personas.size(); i++) {
+            TableRow fila = new TableRow(this);
+            TextView c1 = new TextView(this);
+            TextView c2 = new TextView(this);
+            TextView c3 = new TextView(this);
+            TextView c4 = new TextView(this);
+
+            c1.setText(String.valueOf(i+1));
+            c2.setText(personas.get(i).getCedula());
+            c3.setText(personas.get(i).getNombre());
+            c4.setText(personas.get(i).getApellido());
+
+            fila.addView(c1);
+            fila.addView(c2);
+            fila.addView(c3);
+            fila.addView(c4);
+
+            tabla.addView(fila);
+        }
     }
 }
--- app/src/main/java/com/example/android/personasmovil/Registrar.java
+++ app/src/main/java/com/example/android/personasmovil/Registrar.java
@@ -2,13 +2,47 @@ package com.example.android.personasmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registrar extends AppCompatActivity {
+
+    private TextView txtCedula, txtNombre, txtApellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        txtCedula = findViewById(R.id.txtCedula);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+    }
+
+    public void guardar(View v){
+        String ced, nomb, apell;
+
+        ced = txtCedula.getText().toString();
+        nomb = txtNombre.getText().toString();
+        apell = txtApellido.getText().toString();
+
+        Persona p = new Persona(ced, nomb, apell);
+        p.guardar();
+
+        Toast.makeText(this, getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+        borrar();
+    }
+
+    public void limpiar(View v){
+        borrar();
+    }
+
+    private void borrar(){
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtApellido.setText("");
+        txtCedula.requestFocus();
     }
 }
